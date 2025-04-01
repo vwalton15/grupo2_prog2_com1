6f92a3b74a913845b250d0797a392145377605ad
felichiloteguy <fchiloteguy@udesa.edu.ar>
2025-04-01 16:50:47 -0300

    data base de usuario

---
+++ grupo2_prog2_com1/db/infoUsuarios.js
@@ -0,0 +1,14 @@
+const infoUsuario = {
+    usuario : {
+        email: "emiliamernes@gmail.com",
+        usuario: "emilia_mernes",
+        contrasenia: "jagger",
+        fechaNacimiento: "2000-12-02",
+        nroDocumento: "42956902",
+        fotoPerfil: '/images/fotoPerfil.jpg'
+      }
+      
+}
+
+module.exports= infoUsuario;
+  
