c36263856be5bd4c1e74c6a81ba947567eef56a2
vwalton15 <vwalton@udesa.edu.ar>
2025-04-01 21:21:22 -0300

    Corregi algunas ubicaciones de las imagenes. empece a agregar los partials para el headerLogueado

--- grupo2_prog2_com1/db/product.js
+++ grupo2_prog2_com1/db/product.js
@@ -1,63 +1,64 @@
- const products=[
-    {
+ const products= {
+    lista: [ {
         id: 1,  
         name: "Anillo Doble ",
         descripcion: "Anillo cubierto de oro, combinado con perlas naturales",
-        Image: "/images/productsK/anilloDoble.jpg",
+        Image: "/images/products/anilloDoble.jpg",
     },
     {
         id: 2,  
         name: "Anillo Heart",
         descripcion: "Anillo cubierto de oro, con corazoncitos ",
-        Image: "/images/productsK/anilloHeart.jpg",
+        Image: "/images/products/anilloHeart.jpg",
     },
     {
         id: 3,  
-        name: "aritos Amalfi ",
+        name: "Aritos Amalfi",
         descripcion: "Aritos con perlas naturales",
-        Image: "/images/productsK/aritosAmalfi.jpg",
+        Image: "/images/products/aritosAmalfi.jpg",
     },
     {
         id: 4,
         name: "Collar Amalfi",
         descripcion: "Collar con perlas naturales, perfecto para combinar con el anillo doble y los aritos",
-        Image: "/images/productsK/collarAmalfi.jpg",
+        Image: "/images/products/collarAmalfi.jpg",
     },
     {
         id: 5,
         name: "Collar Heart",
         descripcion: "Collar cubierto de oro con un carazon",
-        Image: "/images/productsK/collarHeart.jpg",
+        Image: "/images/products/collarHeart.jpg",
     },
     {
         id: 6,
         name: "Collar Pearl",
         descripcion: "collar chonky con perlas y charms",
-        Image: "/images/productsK/collarPearl.jpg",
+        Image: "/images/products/collarPearl.jpg",
     },
     {
         id: 7,
         name: "Collar Rapunzel",
         descripcion: "Collar delicado con una flor al estilo rapunzel en el centro",
-        Image: "/images/productsK/collarRapunzel.jpg",
+        Image: "/images/products/collarRapunzel.jpg",
     },
     {
         id: 8,
         name: "Combo Aurora",
         descripcion: "Combo de anillos dorados como la princesa",
-        Image: "/images/productsK/comboAurora.jpg",
+        Image: "/images/products/comboAurora.jpg",
     },
     {
         id: 9,
         name: "Combo Dorado",
         descripcion: "Combo de pulseras delicadas doradas",
-        image:"/images/productsK/comboDorado.jpg",
+        image:"/images/products/comboDorado.jpg",
     },
     {
         id: 10,
         name: "Relos Mar",
         descripcion: "Reloj con detalles en azul y dorado",
-        image:"/images/productsK/relojMar.jpg",
+        image:"/images/products/relojMar.jpg",
     }
-];
+]
+ };
 module.exports= products;
