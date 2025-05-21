c3bba0c56245718639e9eb45b4789692893b637d
felichiloteguy <fchiloteguy@udesa.edu.ar>
2025-05-21 16:09:51 -0300

    haciendo modelo usuarios

---
+++ grupo2_prog2_com1/database/models/Comentario.js
@@ -0,0 +1,25 @@
+module.exports = function (sequelize, dataTypes) {
+    let alias = "Comentario";
+    let cols = {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER.UNSIGNED,
+        },
+       comentario: {
+        type: dataTypes.INTEGER.TEXT,
+       },
+       created_at: {
+        type: dataTypes.DATE,
+       },
+       updated_at: {
+        type: dataTypes.DATE,
+       },
+    }
+    let config = {
+        tableName: "comentarios",
+        timestamps: false,
+    }
+    let Comentario = sequelize.define(alias, cols, config)
+    return Comentario
+}
