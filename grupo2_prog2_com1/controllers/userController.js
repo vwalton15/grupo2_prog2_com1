c4169ad278bc3b5ddc41da8427dfb8a21fd5ea34
vwalton15 <vwalton@udesa.edu.ar>
2025-05-21 17:34:37 -0300

    Login

--- grupo2_prog2_com1/controllers/userController.js
+++ grupo2_prog2_com1/controllers/userController.js
@@ -1,18 +1,58 @@
-const usuario = require("../db/product");
-const listaProductos = require("../db/product");
+const db = require("../database/models");
+let op = db.Sequelize.Op;
+const bcryptjs = require("bcryptjs");
 
 const userController = {
+    index: function (req, res) {
+        db.User.findAll({
+        include:[
+            {association: 'productos'},
+            {association: 'comentarios'}
+        ]
+        })
+        .then(function(resultados){
+                return res.render("index", {productos: resultados});
+            })
+            .catch(function(error){
+                return res.send(error);
+            });
+    },
+
     login: function (req, res) {
-        res.render("login")
+        let userInfo = {
+            email: req.body.email,
+            contrasenia: req.body.contrasenia,
+            recordarme: req.body.recordarme,
+        }
+        db.User.findOne({
+            where: {
+                email: userInfo.email,
+            }
+        })
+        .then(function(user) {
+            if (!user) {
+                return res.send("Email no registrado");
+            }
+
+            let validarC = bcryptjs.compareSync(userInfo.contrasenia, user.contrasenia);
+            if (!validarC) {
+                return res.send("Contraseña incorrecta");
+            }
+            req.session.user = user;
+
+            if (recordarme) {
+                res.cookie("user", user, { maxAge: 60000 }); 
+            }
+
+            return res.redirect("/profile"); 
+        })
+        .catch(function(err) {
+            return res.send(err);
+        });
     },
     register: function (req, res) {
         res.render("register")
     },
-    perfil: function (req, res) {
-        let usuarioLogueado = usuario.usuario;
-        let productos= listaProductos.lista;
-        res.render("profile", { usuario: usuarioLogueado , productos: productos})
-    }
 }
 
 module.exports = userController;
--- grupo2_prog2_com1/database/models/Usuario.js
+++ grupo2_prog2_com1/database/models/Usuario.js
@@ -39,5 +39,15 @@ module.exports = function (sequelize, dataTypes) {
 
     const Usuario = sequelize.define(alias, cols, config);
 
+    Usuario.associate = function (models) {
+        Usuario.hasMany(models.Producto, {
+            as: 'productos',
+            foreignKey: 'usuario_id'
+        });
+        Usuario.hasMany(models.Comentario, {
+            as: 'comentarios',
+            foreignKey: 'usuario_id'
+        });
+    }
     return Usuario;
 }
