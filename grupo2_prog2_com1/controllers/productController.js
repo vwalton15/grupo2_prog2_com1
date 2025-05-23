e494b16a38ae547739ba4db5b9cba6be1cb111dd
Onabeitia <onabeitia@icloud.com>
2025-05-23 11:42:08 -0300

    search results -- falta ultimar detalles y ver que todo funcione okay

--- grupo2_prog2_com1/controllers/productController.js
+++ grupo2_prog2_com1/controllers/productController.js
@@ -9,6 +9,25 @@ const productoController = {
         let usuarioInfo= productos.usuario;
         res.render('product', { producto: productoFijo, usuario: usuarioInfo }); 
     },
+    search: function (req, res) {
+        const searchTerm = req.query.q; 
+        if (!searchTerm) {
+            return res.render('searchResults', { products: [], message: "Ingrese un término de búsqueda." });
+        }
+
+    
+        const resultados = productos.lista.filter(function(producto) {
+            return producto.name.toLowerCase().includes(searchTerm.toLowerCase());
+        });
+
+        if (resultados.length === 0) {
+            return res.render('searchResults', { products: [], message: "No hay resultados para su criterio de búsqueda." });
+        }
+
+        res.render('searchResults', { products: resultados, message: null });
+    }
 };
 
 module.exports = productoController;
+
+
--- grupo2_prog2_com1/routes/users.js
+++ grupo2_prog2_com1/routes/users.js
@@ -8,5 +8,7 @@ router.get('/login', userController.login);
 router.get('/profile', userController.perfil);
 //router.get('/logout', userController.logout);
 
+router.get('/search', productController.search); 
+
 
 module.exports = router;
